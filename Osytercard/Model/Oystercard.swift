67ae930af817ef5eb7231c3c7fcd4f9cd10e543a
Andrew <24258342+acodeguy@users.noreply.github.com>
2021-02-21 14:03:34 +0000

    Balance has a maximum amount

--- Osytercard/Model/Oystercard.swift
+++ Osytercard/Model/Oystercard.swift
@@ -6,8 +6,20 @@ class Oystercard {
     /// The current balance of this card
     var balance: Double = 0
 
+    /// The maximum balance allowed
+    private let maximumBalance: Double = 90.00
+
     /// Increase this card's balance by the supplied amount
-    func topup(_ amount: Double) {
-        balance += amount
+    func topup(_ amount: Double) throws {
+        let projectedBalance = balance + amount
+        if projectedBalance < maximumBalance {
+            balance += amount
+        } else {
+            throw TopupError.maximumBalanceExceeded
+        }
     }
+}
+
+enum TopupError: Error {
+    case maximumBalanceExceeded
 }
--- OystercardTests/OystercardTests.swift
+++ OystercardTests/OystercardTests.swift
@@ -3,11 +3,19 @@ import XCTest
 
 class OystercardTests: XCTestCase {
 
-    func testToppingUpFundsIncreasesTheBalanceByTheCorrectAmount() {
+    func testToppingUpFundsIncreasesTheBalanceByTheCorrectAmount() throws {
         let card = Oystercard()
 
-        card.topup(5.50)
+        try card.topup(5.50)
 
         XCTAssertEqual(card.balance, 5.50)
+    }
+
+    func testToppingUpFundsFailsIfBalanceWouldBeGreaterThanTheMaximumAllowed() throws {
+        let card = Oystercard()
+
+        XCTAssertThrowsError(try card.topup(90.01), "Balance would exceed maximum allowed") { error in
+            XCTAssertEqual(card.balance, 0)
+        }
     }
 }

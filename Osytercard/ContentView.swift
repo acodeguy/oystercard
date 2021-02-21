b980d2ac7a81ed4faf590e3925cd2031f098139d
Andrew <24258342+acodeguy@users.noreply.github.com>
2021-02-21 13:28:21 +0000

    Initial Commit

---
+++ Osytercard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Osytercard
+//
+//  Created by A. on 21/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Osytercard/OsytercardApp.swift
@@ -0,0 +1,17 @@
+//
+//  OsytercardApp.swift
+//  Osytercard
+//
+//  Created by A. on 21/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct OsytercardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
